de4ed5e19807907fd8bde481bb02615e5b813f85
daniil148852 <daniildan850@gmail.com>
2026-04-07 16:19:27 +0200

    Update GravityPull.java

--- src/main/java/com/gravitypull/GravityPull.java
+++ src/main/java/com/gravitypull/GravityPull.java
@@ -1,24 +1,32 @@
-package com.gravitypull;
+package gravity;
 
+import gravity.mixin.FallingBlockEntityMixin;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+public class GravityMod implements ModInitializer {
+    public static final String MOD_ID = "gravity_mod";
+    
+    // Создаем наш предмет
+    public static final Item GRAVITY_GAUNTLET = new GravityGauntletItem(
+            new FabricItemSettings().maxDamage(1000).rarity(Rarity.EPIC)
+    );
 
-public class GravityPull implements ModInitializer {
-	public static final String MOD_ID = "gravity-pull";
+    @Override
+    public void onInitialize() {
+        // Регистрируем предмет
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, "gravity_gauntlet"), GRAVITY_GAUNTLET);
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-	}
+        // Добавляем в вкладку "Инструменты" в креативе
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(content -> {
+            content.add(GRAVITY_GAUNTLET);
+        });
+    }
 }
